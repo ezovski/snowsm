dbd5c2743232946802c4c02e15dc3aba83c9f90f
Matt Ezovski <matt.ezovski@jumptap.com>
2016-05-17 00:04:02 -0400

    miscellaneous stuff

--- process_trails.py
+++ process_trails.py
@@ -3,7 +3,7 @@ from database import db
 import update_ski_areas
 __author__ = 'mattezovski'
 
-from shapely import geometry
+from shapely import geometry, wkb
 
 import overpass
 
@@ -32,8 +32,11 @@ def build_db(geoId):
     # New Hampshire and some surrounding areas
     BOUNDING_BOX = '43.12103377575541,-73.7237548828125,44.797428998555674,-69.378662109375'
 
+    # Boyne Highlands
+    # BOUNDING_BOX = '45,-85,46,-84'
+
     if geoId:
-        BOUNDING_BOX = BB_LIST[int(geoId)]
+       BOUNDING_BOX = BB_LIST[int(geoId)]
 
     # Core of White Mountains
     #BOUNDING_BOX = '44.00269350325321,-71.64871215820312,44.36067856998804,-71.12411499023438'
@@ -81,8 +84,10 @@ def build_db(geoId):
             continue
         exists = Trail.query.filter_by(osm_id=trail['id'])
         if exists.count():
+            new_trail = False
             t = exists[0]
         else:
+            new_trail = True
             t= Trail()
 
         t.name = trail['properties'].get('name')
@@ -90,11 +95,11 @@ def build_db(geoId):
         t.osm_id = trail['id']
         t.path = str(geometry.LineString(trail['geometry']['coordinates']))
 
-        if not exists.count():
+        if new_trail:
             db.session.add(t)
         db.session.commit()
 
-        if not exists.count():
+        if not new_trail:
             # Remaining steps only apply to new trails
             continue
 
@@ -109,32 +114,14 @@ def build_db(geoId):
             continue
 
         # if results.count() != 1:
-        results = find_nearest_ski_area(t)
+        results = find_nearest_ski_area(t, api)
 
         if results:
             t.ski_area_id = results
             continue
 
-        # Attempt to locate and create a different kind of ski area.
-        lat_lon = str(trail.geometry.coordinates[0][1]) + ',' + str(trail.geometry.coordinates[0][0])
-        sc_query = 'node(around:1700,' + lat_lon + ')[leisure=sports_centre];'
-        sports_centre_response = api.Get(sc_query, 'json')
-        if len(sports_centre_response.features) > 0:
-            if len(sports_centre_response.features) > 1:
-                print('way ' + str(trail['id']) + ' is confused')
-            sa = sports_centre_response.features[0]
-            sam = SkiArea()
-            sam.inferred = True
-            sam.name = sa['properties'].get('name')
-            if not sam.name:
-                continue
-            sam.osm_id = sa['id']
-            sam.boundary = str(geometry.Point(sa['geometry']['coordinates']).buffer(0.020))
-            #sam.boundary = str(geometry.Point(sa['geometry']['coordinates']))
-            db.session.add(sam)
-            t.ski_area = sam
-            # t.ski_area = sam
-            db.session.commit()
+        # t.ski_area = sam
+        db.session.commit()
 
     db.session.commit()
 
@@ -161,7 +148,7 @@ def build_db(geoId):
             lt.ski_area_id = results[0].id
             continue
 
-        results = find_nearest_ski_area(lt)
+        results = find_nearest_ski_area(lt, api)
         if results:
             lt.ski_area_id = results
 
@@ -220,7 +207,7 @@ def build_db(geoId):
     print 'done'
 
 
-def find_nearest_ski_area(entity):
+def find_nearest_ski_area(entity, api=None):
 
     results = db.session.query(Trail).filter(Trail.ski_area_id != None, Trail.path.ST_Distance(entity.path) < .005)
 
@@ -238,5 +225,28 @@ def find_nearest_ski_area(entity):
     if results.count() > 0:
 
         return results[0].id
+
+    if not api:
+        return None
+    # Attempt to locate and create a different kind of ski area.
+    the_path = wkb.loads(bytes(entity.path.data)).coords.xy
+    lat_lon = str(the_path[1][0]) + ',' + str(the_path[0][0])
+    sc_query = 'node(around:1700,' + lat_lon + ')[leisure=sports_centre];'
+    sports_centre_response = api.Get(sc_query, 'json')
+    if len(sports_centre_response.features) > 0:
+        if len(sports_centre_response.features) > 1:
+            print('way ' + str(entity.id) + ' is confused')
+        sa = sports_centre_response.features[0]
+        sam = SkiArea()
+        sam.inferred = True
+        sam.name = sa['properties'].get('name')
+        if not sam.name:
+            return None
+        sam.osm_id = sa['id']
+        sam.boundary = str(geometry.Point(sa['geometry']['coordinates']).buffer(0.020))
+        #sam.boundary = str(geometry.Point(sa['geometry']['coordinates']))
+        db.session.add(sam)
+        db.session.commit()
+        return sam.id
 
     return None
